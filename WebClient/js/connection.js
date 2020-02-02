f7bf703bc900474170d0ea94e2c34ba2cf1dbced
Sergey Burov <sergey.burov.ext@siemens.com>
2020-02-02 18:09:17 +0400

    Refactored web client, added mouse position display.

--- WebClient/js/connection.js
+++ WebClient/js/connection.js
@@ -1,28 +1,23 @@
 var wsUri = "ws://localhost:8080/";
 var websocket = new WebSocket(wsUri);
-  
-websocket.onopen = function(evt)
-{
+
+websocket.onopen = function (evt) {
 	console.log("CONNECTED");
 }
 
-websocket.onclose = function(evt)
-{
+websocket.onclose = function (evt) {
 	console.log("DISCONNECTED");
 }
 
-websocket.onmessage = function(evt)
-{
+websocket.onmessage = function (evt) {
 	SetCharacters(JSON.parse(evt.data));
 }
 
-websocket.onerror = function(evt)
-{
+websocket.onerror = function (evt) {
 	console.log('ERROR:' + evt.data);
 }
 
-function SendToServer(message)
-{
+function SendToServer(message) {
 	console.log("SENT: " + message);
 	websocket.send(message);
 }
