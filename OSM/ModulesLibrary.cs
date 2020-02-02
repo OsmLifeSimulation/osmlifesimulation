285946df1ce6126b20412db693c2ab9f73c6a7c8
Sergey Burov <sergey.burov.ext@siemens.com>
2020-02-02 18:10:34 +0400

    Improved modules library, added initialization order attribute.

--- OSM/ModulesLibrary.cs
+++ OSM/ModulesLibrary.cs
@@ -3,7 +3,9 @@ using OSMLSGlobalLibrary.Modules;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Runtime.Loader;
 
 namespace OSM
 {
@@ -13,25 +15,36 @@ namespace OSM
 
         public ModulesLibrary(OsmXml rawData, MapObjectsCollection mapObjects)
         {
+            var assemblies = new List<Assembly>();
             foreach (string file in Directory.EnumerateFiles(Constants.ModulesPath, "*.dll"))
             {
                 try
                 {
-                    var DLL = Assembly.LoadFile(Path.GetFullPath(file));
-
-                    foreach (Type type in DLL.GetExportedTypes())
-                    {
-                        if (type.IsSubclassOf(typeof(OSMLSModule)))
-                        {
-                            modules[type.Name] = (OSMLSModule)Activator.CreateInstance(type, rawData, modules, mapObjects);
-                        }
-                    }
+                    assemblies.Add(AssemblyLoadContext.Default.LoadFromAssemblyPath(Path.GetFullPath(file)));
                 }
                 catch (Exception exeption)
                 {
-                    Console.WriteLine("DLL " + file + " can't be loaded as a module. Inner Exception:\n" + exeption.InnerException);
-                    Console.WriteLine("Press Enter to continue...");
-                    Console.ReadLine();
+                    Console.WriteLine($"DLL {file} can't be loaded. Exception:\n{exeption}");
+                }
+            }
+
+            var moduleTypes = assemblies
+                .SelectMany(a => a.GetExportedTypes())
+                .Where(type => type.IsSubclassOf(typeof(OSMLSModule)))
+                .OrderBy(type => 
+                    ((CustomInitializationOrderAttribute)type.GetCustomAttributes(typeof(CustomInitializationOrderAttribute), false).FirstOrDefault())
+                        ?.InitializationOrder ?? 0
+                );
+
+            foreach (var type in moduleTypes)
+            {
+                try
+                {
+                    modules[type.Name] = (OSMLSModule)Activator.CreateInstance(type, rawData, modules, mapObjects);
+                }
+                catch (Exception exeption)
+                {
+                    Console.WriteLine($"Module {type.Name} can't be loaded. Exception:\n{exeption}");
                 }
             }
         }
---
+++ OSMLSGlobalLibrary/Modules/CustomInitializationOrderAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSMLSGlobalLibrary.Modules
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CustomInitializationOrderAttribute : Attribute
+    {
+        public double InitializationOrder { get; }
+        public CustomInitializationOrderAttribute(double initializationOrder = 0)
+        {
+            InitializationOrder = initializationOrder;
+        }
+    }
+}
